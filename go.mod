da217cd7e7d2ee2b7b4c0e9b028995454ddc2a23
Yaser Sharifi <yaser.raadco@gmail.com>
2025-11-23 17:49:35 +0330

    feat: add the user model

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-shop-api
+
+go 1.25.4
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
+)
---
+++ internal/models/user.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type UserRole string
+
+const (
+	RoleAdmin     UserRole = "admin"
+	RoleUser      UserRole = "user"
+	RoleManager   UserRole = "manager"
+	RoleSeller    UserRole = "seller"
+	RoleSupporter UserRole = "supporter"
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	FirstName string    `gorm:"type:varchar(100)"`
+	LastName  string    `gorm:"type:varchar(100)"`
+	Email     string    `gorm:"uniqueIndex;not null"`
+	Password  string
+	Phone     string   `gorm:"type:varchar(15);uniqueIndex"`
+	Role      UserRole `gorm:"type:varchar(20);not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+type UserInput struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email" binding:"required,email"`
+	Phone     string `json:"phone" binding:"min=10,max=15"`
+	Password  string `json:"password" binding:"required,min=8"`
+}
+
+type UserOutput struct {
+	ID        string    `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	Role      UserRole  `json:"role"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (u *User) SetPassword(raw string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(raw), 12)
+	if err != nil {
+		return  err
+	}
+
+	u.Password = string(hashed)
+	return nil
+}
+
+func (u *User) CheckPassword(raw string) bool {
+
+}
